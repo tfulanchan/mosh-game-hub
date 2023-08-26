7a13edb4ec78ad7b1a3831b1fca47586e44075a9
tfulanchan <60479975+tfulanchan@users.noreply.github.com>
2023-08-26 09:18:23 -0400

    Build game cards

--- src/hooks/useGames.ts
+++ src/hooks/useGames.ts
@@ -1,10 +1,11 @@
+import { CanceledError } from "axios";
 import { useEffect, useState } from "react";
 import apiClient from "../services/api-clients";
-import { CanceledError } from "axios";
 
-interface Game {
+export interface Game {
   id: number;
   name: string;
+  background_image: string;
 }
 
 interface FetchGamesResponse {
