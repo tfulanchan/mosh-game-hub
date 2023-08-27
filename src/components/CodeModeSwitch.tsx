3b52069b8caa4b715cf7a1d9b4d797fc7676074d
tfulanchan <60479975+tfulanchan@users.noreply.github.com>
2023-08-27 12:26:44 -0400

    add searchbar and search icon

--- src/components/CodeModeSwitch.tsx
+++ src/components/CodeModeSwitch.tsx
@@ -8,7 +8,7 @@ const CodeModeSwitch = () => {
   return (
     <HStack>
         <Switch colorScheme='green' isChecked={colorMode === 'dark'} onChange={toggleColorMode} />
-        <Text>Dark Mode</Text>
+        <Text whiteSpace='nowrap' >Dark Mode</Text>
     </HStack>
   )
 }
